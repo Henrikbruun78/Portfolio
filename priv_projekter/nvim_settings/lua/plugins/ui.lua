b4f3da31e158976d6c264f122b42a2c8832a22f2
Henrikbruun78 <henrikbaltzerbruun78@gmail.com>
2026-03-15 01:49:08 +0100

    Update ui.lua
    added space+ff for finding files in telescope

--- priv_projekter/nvim_settings/lua/plugins/ui.lua
+++ priv_projekter/nvim_settings/lua/plugins/ui.lua
@@ -115,9 +115,14 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    },
     config = function()
       local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
 
       telescope.setup({
         defaults = {
@@ -138,11 +143,6 @@ return {
           },
         },
       })
-
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
     end,
   },
   {
