0c22296705d37d9b5ef8dc57fafa3c1b8ba5d8be
Henrikbruun78 <henrikbaltzerbruun78@gmail.com>
2026-03-15 01:39:34 +0100

    Update ui.lua
    added telescope for faster file access

--- priv_projekter/nvim_settings/lua/plugins/ui.lua
+++ priv_projekter/nvim_settings/lua/plugins/ui.lua
@@ -109,6 +109,43 @@ return {
     lazy = true,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = {
+            prompt_position = "top",
+            preview_width = 0.55,
+          },
+          sorting_strategy = "ascending",
+          winblend = 0,
+          prompt_prefix = "  ",
+          selection_caret = "  ",
+          path_display = { "smart" },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+    end,
+  },
+  {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
